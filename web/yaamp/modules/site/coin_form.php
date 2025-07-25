587a4b5a65ff6f55cf36803c50b6a756d2fe5b76
msy2008 <963858700@qq.com>
2025-07-26 00:14:24 +0800

    Update coin_form.php

--- web/yaamp/modules/site/coin_form.php
+++ web/yaamp/modules/site/coin_form.php
@@ -235,7 +235,7 @@ echo CUFHtml::openActiveCtrlHolder($coin, 'usesegwit');
 echo CUFHtml::activeLabelEx($coin, 'usesegwit');
 echo CUFHtml::activeCheckBox($coin, 'usesegwit');
 echo '<p class="formHint2"></p>';
-echo CUFHtml::closeCtrlHolder
+echo CUFHtml::closeCtrlHolder();
 
 echo "</div>";
 
